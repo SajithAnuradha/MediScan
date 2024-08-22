652ec0f5d5fce65540db7ffe7e55ab26ed39e2ff
Sajith <sajithanuradha860@gmail.com>
2024-08-22 22:45:11 +0530

    add the login

--- Frontend/mediscan/lib/features/auth/repository/auth_remote_repository.dart
+++ Frontend/mediscan/lib/features/auth/repository/auth_remote_repository.dart
@@ -28,7 +28,30 @@ class AuthRemoteRepository {
 
     if (response.statusCode == 200) {
       print('User created');
+    } else {
+      print('User not created');
     }
-    Future<void> login() async {}
+  }
+
+  Future<void> login({
+    required String email,
+    required String password,
+  }) async {
+    final response = await http.post(
+      Uri.parse('http://127.0.0.1:8000/auth/login'),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'email': email,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      print('User logged in');
+    } else {
+      print('User not logged in');
+    }
   }
 }
